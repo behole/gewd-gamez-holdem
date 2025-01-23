1878313723f268ea0377d84f3b1b0ef19d20e652
behole <behole@gmail.com>
2025-01-23 12:39:30 -0800

    Add initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokerTable from './components/game/PokerTable';
+
+function App() {
+  return (
+    <div className="App">
+      <PokerTable />
+    </div>
+  );
+}
+
+export default App;
