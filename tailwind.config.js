8fd9d44df545688e15bff6645ccf44f67bb6152c
behole <behole@gmail.com>
2025-01-23 12:39:08 -0800

    Add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // Custom color palette can be added here
+      },
+    },
+  },
+  plugins: [],
+}
