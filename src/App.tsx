cefffe1edacad8406348284be4030de28f254c43
behole <behole@gmail.com>
2025-01-23 13:31:14 -0800

    Add App component with proper styling

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PokerTable } from './components/game/PokerTable'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <main className="container mx-auto px-4 py-8">
+        <PokerTable />
+      </main>
+    </div>
+  )
+}
+
+export default App
